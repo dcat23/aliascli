db6c498ff377e790a2a96202d3bb217da11792e6
Devin Catuns <devincatuns1@gmail.com>
2023-03-18 13:31:12 -0500

    fix delete read command

--- alias_cli.sh
+++ alias_cli.sh
@@ -1,53 +1,50 @@
+export ALIAS_COMMANDS_FILE="$HOME/AliasCLI/aliases"
+source "$ALIAS_COMMANDS_FILE"                                                                                          
+
 function al() {
     if [[ $1 == "-h" ]]; then
-        echo "al -h          : show this help page"
-        echo "al             : list all aliases"
-        echo "al <string>    : list aliases containing <string>"
-        echo "al -d <string> : delete aliases containing <string>"
-        echo "al -D <string> : delete aliases containing <string> without confirmation"
-        echo "al -v <string> : view command for an alias"
+        echo "al -h                : show this help page"
+        echo "al                   : list all aliases"
+        echo "al <string>          : list aliases containing <string>"
+        echo "al -d <string>       : delete aliases containing <string>"
+        echo "al -v <string>       : view command for an alias"
         echo "al <alias> <command> : create a new alias or update an existing one"
         return 0
     fi
 
     if [[ $# -eq 0 ]]; then
-        grep "^alias" ~/.zsh_aliases | sed 's/^alias //g'
+        grep "^alias" "$ALIAS_COMMANDS_FILE" | sed 's/^alias //g'
     elif [[ $# -eq 1 ]]; then
-        grep "$1" ~/.zsh_aliases | sed 's/^alias //g'
-    elif [[ $1 == "-d" ]]; then
-        if [[ $# -lt 2 ]]; then
-            echo "Please specify an alias to delete"
-            return 1
-        fi
-        if [[ $2 == "-D" ]]; then
-            confirmation="y"
-            shift
-        else
-            read -p "Delete aliases containing \"$2\"? (y/n) " confirmation
-        fi
-        if [[ $confirmation =~ ^[Yy]$ ]]; then
-            sed -i.bak "/^alias .*=$2/d" ~/.zsh_aliases
-            echo "Deleted aliases containing \"$2\""
-        fi
+        grep "$1" "$ALIAS_COMMANDS_FILE" | sed 's/^alias //g'
     elif [[ $1 == "-v" ]]; then
-        command=$(grep "alias $2=" ~/.zsh_aliases | sed "s/alias $2='\(.*\)'$/\1/")
+        command=$(grep "alias $2=" "$ALIAS_COMMANDS_FILE" | sed "s/alias $2='\(.*\)'$/\1/")
         if [[ -n $command ]]; then
             echo "$2 -> $command"
         else
             echo "No command found for alias: \"$2\""
         fi
+    elif [[ $1 =~ ^-[Dd]$ ]]; then
+        if [[ $# -lt 2 ]]; then
+            echo "Please specify an alias to delete"
+            return 1
+        fi
+        echo "Delete alias '$2'? [y/n]" && read
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            sed -i.bak "/^alias $2=.*/d" "$ALIAS_COMMANDS_FILE"
+            echo "Deleted alias $2"
+        fi
     elif [[ $# -eq 2 ]]; then
-        if grep -q "^alias $1=" ~/.zsh_aliases; then
-            existing_command=$(grep "^alias $1=" ~/.zsh_aliases | sed "s/^alias $1='\(.*\)'$/\1/")
+        if grep "^alias $1=" "$ALIAS_COMMANDS_FILE"; then
+            existing_command=$(grep "^alias $1=" "$ALIAS_COMMANDS_FILE" | sed "s/^alias $1='\(.*\)'$/\1/")
             echo "Alias \"$1\" already exists: \"$existing_command\""
-            read -p "Update to \"$2\"? (y/n) " -r
-            echo
+            echo "Update to \"$2\"? (y/n) "
+            read
             if [[ $REPLY =~ ^[Yy]$ ]]; then
-                sed -i.bak "/^alias $1=\"/d" ~/.zsh_aliases
-                echo "alias $1=\"$2\"" >> ~/.zsh_aliases
+                sed -i.bak "/^alias $1=\"/d" "$ALIAS_COMMANDS_FILE"
+                echo "alias $1=\"$2\"" >> "$ALIAS_COMMANDS_FILE"
             fi
         else
-            echo "alias $1=\"$2\"" >> ~/.zsh_aliases
+            echo "alias $1=\"$2\"" >> "$ALIAS_COMMANDS_FILE"
         fi
     else
         echo "Invalid arguments"
